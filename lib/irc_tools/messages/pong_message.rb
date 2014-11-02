35496973582620c5241f7ad3ef823f4a587a0bb8
Mark Glen Fletcher <markglenfletcher@googlemail.com>
2014-11-02 21:04:40 +0000

    Migrate pong message

---
+++ lib/irc_tools/messages/pong_message.rb
@@ -0,0 +1,14 @@
+module IrcTools
+  class PongMessage < Message
+    def initialize(options = {})
+      super(options.merge(:type => :pong))
+      @validator = MessageTypes::PONG_MATCHER
+    end
+
+    protected
+
+    def build_message
+      "PONG #{server}".strip
+    end
+  end
+end
---
+++ test/irc_tools/messages/pong_message_test.rb
@@ -0,0 +1,6 @@
+class PongMessageTest < Minitest::Test
+  def test_to_s_is_correct
+    assert_equal 'PONG', IrcTools::PongMessage.new.to_s
+    assert_equal 'PONG server', IrcTools::PongMessage.new(:server => 'server').to_s
+  end
+end
